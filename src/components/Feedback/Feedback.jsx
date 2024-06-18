7a61171de23bdf0c6bf05ec764c5a0299a2857d4
Aleksandr <algryshko@gmail.com>
2024-06-18 22:35:40 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -8,7 +8,7 @@ export default function Feedback({ count: { good, neutral, bad }, total, positiv
           <p className={css.feedbackItem}>Good: {good}</p>
           <p className={css.feedbackItem}>Neutral: {neutral}</p>
           <p className={css.feedbackItem}>Bad: {bad}</p>
-          <p className={css.feedbackItem}>Positive: {positive}</p>
+          <p className={css.feedbackItem}>Positive: {positive}%</p>
         </>
       ) : (
         <p className={css.noFeedback}>No feedback yet</p>
